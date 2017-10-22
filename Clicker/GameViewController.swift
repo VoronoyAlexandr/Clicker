bc25655b2d9f151b174e7a72e9b7bb44ea8454cf
Alexandr Voronoy <servermed@gmail.com>
2017-10-22 13:10:17 +0300

    Game init

---
+++ Clicker/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Clicker
+//
+//  Created by Sasha Voronoy on 22.10.2017.
+//  Copyright © 2017 Sasha Voronoy. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+}
